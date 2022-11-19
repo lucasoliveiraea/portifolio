aff0078893b3af9408b02dd9a6459018e40f89dc
Lucas Oliveira <lucasoliveiraea@hotmail.com>
2022-11-18 21:48:52 -0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+function abrirmenu() {
+    let barraMenuAberto = document.getElementById('barra-menu')
+
+    if (barraMenuAberto.style.width == "0px") {
+        barraMenuAberto.style.width = "180px";
+    } else {
+        barraMenuAberto.style.width = "0px";
+    }
+}
